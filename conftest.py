8a1365bdd6646a7ff110b9a8341098324fd401bb
nandakumar <nandakumar.choodamani@gmail.com>
2024-05-08 15:50:34 +0530

    Added conftest file: may 08

--- conftest.py
+++ conftest.py
@@ -2,13 +2,15 @@ import pytest
 
 @pytest.fixture(autouse= True)
 def init_config():
-    print("1_Open_browser")
-    print("1_open_udl")
-    print("1_input_credentials")
-    print("Performa tasks and complete")
+    print("Perform: initial configuration.")
+    print("Perform: 1_Open_browser")
+    print("Perform: 1_open_udl")
+    print("Perform: 1_input_credentials")
+    print("Perform: tasks and complete")
     yield
-    print("1_Complete and close tasks")
-    print("1_close the url")
-    print("1_close the browser")
+    print("\nRemove auto configuration.") 
+    print("Remove: 1_Complete and close tasks")
+    print("Remove: 1_close the url")
+    print("Remove: 1_close the browser")
 
 
